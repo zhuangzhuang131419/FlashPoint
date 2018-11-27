b85d5b99b89aabe47c08dd3188c35bf4ffcf5ffa
Dean_Jin <qq1162346256@sina.com>
2018-11-27 08:29:00 -0500

    In game level added

---
+++ Flash_Point/Source/Flash_Point/FPPlayerController.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "FPPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FLASH_POINT_API AFPPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
