7775850341c8a0af5e926f1c1f9deaa6c4bee111
Zhuang <zy22092@nottingham.edu.cn>
2019-03-07 16:10:39 -0500

    Add door Onclicked event in C++

--- Flash_Point/Source/Flash_Point/Door.cpp
+++ Flash_Point/Source/Flash_Point/Door.cpp
@@ -1,6 +1,8 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "Door.h"
+#include "Engine/World.h"
+#include "FPPlayerController.h"
 
 ADoor::ADoor() 
 {
@@ -10,12 +12,46 @@ ADoor::ADoor()
 	DoorFrame->AttachToComponent(WallMesh, FAttachmentTransformRules::KeepRelativeTransform, FName("DoorFrame"));
 	Door->AttachToComponent(DoorFrame, FAttachmentTransformRules::KeepRelativeTransform, FName("Door"));
 	
-	// isBlocked = true;
+	isBlocked = true;
 	edgeType = EEdgeType::Door;
+}
+
+bool ADoor::IsOpened()
+{
+	return isOpened;
+}
+
+void ADoor::SetIsOpend(bool openStatus)
+{
+	isOpened = openStatus;
 }
 
 void ADoor::BeginPlay()
 {
 	Super::BeginPlay();
+	// Create binding to on cursor click
+	FScriptDelegate onMouseClickedDel;
+	onMouseClickedDel.BindUFunction(this, "OnDoorClicked");
+	OnClicked.Add(onMouseClickedDel);
+}
+
+void ADoor::OnDoorClicked(AActor* Target, FKey ButtonPressed)
+{
+	if (ButtonPressed != FKey("LeftMouseButton")) return;
+	UE_LOG(LogTemp, Warning, TEXT("A door has been clicked."));
+	AFPPlayerController* playerController = Cast<AFPPlayerController>(GetWorld()->GetFirstPlayerController());
+	if (playerController)
+	{
+		if (playerController->GetCurrentOperation() == EGameOperations::OpenDoor) {
+			if (isOpened)
+			{
+				CloseDoor();
+			}
+			else
+			{
+				OpenDoor();
+			}
+		}
+	}
 }
 
