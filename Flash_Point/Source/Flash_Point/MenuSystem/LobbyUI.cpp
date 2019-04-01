e0143efae6adcf6932023cd72a3a6a3c5a337297
Dean_Jin <qq1162346256@sina.com>
2019-04-01 17:45:01 -0400

    Refactoring Lobby widgets for actually lobby joining

--- Flash_Point/Source/Flash_Point/MenuSystem/LobbyUI.cpp
+++ Flash_Point/Source/Flash_Point/MenuSystem/LobbyUI.cpp
@@ -1,6 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "LobbyUI.h"
+#include "MenuSystem/ChatWidget.h"
 
 ULobbyUI::ULobbyUI(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer) {
 
@@ -8,8 +9,14 @@ ULobbyUI::ULobbyUI(const FObjectInitializer& ObjectInitializer) : Super(ObjectIn
 
 void ULobbyUI::BindChatManagerWithUI(AChatManager * inMan)
 {
+	if (ensure(ChatPanel)) {
+		ChatPanel->BindChatManager(inMan);
+	}
 }
 
 void ULobbyUI::RelateChatUIWithPlayer(AFPPlayerController * inPlayer)
 {
+	if (ensure(ChatPanel)) {
+		ChatPanel->SetRelatedPlayer(inPlayer);
+	}
 }
